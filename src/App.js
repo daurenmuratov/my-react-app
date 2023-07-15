b6222f60f1d74dc690b52e1dd14ba36f48a887e6
daurenmuratov <daurenmuratov@gmail.com>
2023-07-15 14:00:22 +0600

    add OK text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Deployment is happening here!</p>
+          <p>Deployment is happening here! OK</p>
         </div>
       </div>
     );
